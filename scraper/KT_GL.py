12a86d77c78f3bcc39dc038b1c7b26102bc8e6c9
David Oesch <david.oesch@gmail.com>
2022-10-29 12:27:26 +0200

    added KT_GL

--- scraper/KT_GL.py
+++ scraper/KT_GL.py
@@ -1,10 +1,11 @@
 #Create OWSLIB configfile 
+#URSL according to https://www.gl.ch/verwaltung/bau-und-umwelt/hochbau/raumentwicklung-und-geoinformation/geoportal-kanton-glarus.html/808
 
 #SERVICE WMS
 def scrape(source,service,i,layertree, group,layer_data,prefix):
-    type=service.identification.type
+    type=service.url #since Galrus configured the Service not according to OGC , we use the
  
-    if "WMS" in type:
+    if "wms" in type:
         layer_data["OWNER"]= source['Description']
         layer_data["TITLE"]= service.contents[i].title
         layer_data["NAME"]= service.contents[i].name
@@ -13,7 +14,7 @@ def scrape(source,service,i,layertree, group,layer_data,prefix):
         layer_data["ABSTRACT"]= service.contents[i].abstract
         layer_data["KEYWORDS"]= ", ".join(service.contents[i].keywords+service.identification.keywords)
         layer_data["LEGEND"]= service.contents[i].styles['default']['legend'] if 'default' in service.contents[i].styles.keys() else ""
-        layer_data["CONTACT"]=service.provider.contact
+        layer_data["CONTACT"]=service.provider.contact.email
         layer_data["SERVICELINK"]=service.request
         layer_data["METADATA"]=layer_data["METADATA"]=service.contents[i].metadataUrls[0]['url'] if 0 in service.contents[i].metadataUrls else ""
         layer_data["UPDATE"]=""
@@ -28,28 +29,9 @@ def scrape(source,service,i,layertree, group,layer_data,prefix):
             "%20"+str(layer_data["CENTER_LON"])+"&zoom="+str(layer_data["MAX_ZOOM"])
         return(layer_data)
     elif "WMTS" in type:
-        layer_data["OWNER"]= source['Description']
-        layer_data["TITLE"]= service.contents[i].title
-        layer_data["NAME"]= service.contents[i].name
-        layer_data["TREE"]= layertree
-        layer_data["GROUP"]= group if group != 0 else ""
-        layer_data["ABSTRACT"]= service.identification.abstract+" "+service.identification.accessconstraints
-        layer_data["KEYWORDS"]= ", ".join(service.contents[i].keywords+service.identification.keywords)
-        layer_data["LEGEND"]= service.contents[i].styles['default']['legend'] if 'legend' in service.contents[i].styles.keys() else ""
-        layer_data["CONTACT"]=service.provider.name
-        layer_data["SERVICELINK"]=service.url
-        layer_data["METADATA"]=service.serviceMetadataURL
-        layer_data["UPDATE"]=""
-        layer_data["SERVICETYPE"]=service.identification.type
-        layer_data["MAX_ZOOM"]= 7 #this is the map.geo.admin.ch map zoom at approx 1:20k
-        layer_data["CENTER_LAT"]=(service[i].boundingBoxWGS84[1]+service[i].boundingBoxWGS84[3])/2
-        layer_data["CENTER_LON"]=(service[i].boundingBoxWGS84[0]+service[i].boundingBoxWGS84[2])/2
-        layer_data["BBOX"]=' '.join([str(elem) for elem in (service.contents[i].boundingBoxWGS84)])
-        layer_data["MAPGEO"]= r""+prefix+"layers=WMTS||"+service.contents[i].id+"||"\
-            +service.url+"&swisssearch="+str(layer_data["CENTER_LAT"])+\
-            "%20"+str(layer_data["CENTER_LON"])+"&zoom="+str(layer_data["MAX_ZOOM"])
+        print("STAC detetcted ..add config")
         return(layer_data)
-    elif "WFS" in type:
+    elif "wfs" in type:
         layer_data["OWNER"]= source['Description']
         layer_data["TITLE"]= service.contents[i].title
         layer_data["NAME"]= service.contents[i].id
@@ -59,10 +41,10 @@ def scrape(source,service,i,layertree, group,layer_data,prefix):
         layer_data["KEYWORDS"]= ", ".join(service.contents[i].keywords+service.identification.keywords)
         layer_data["LEGEND"]= ""
         layer_data["CONTACT"]=service.provider.contact.email
-        layer_data["SERVICELINK"]=service.url
+        layer_data["SERVICELINK"]=service.request
         layer_data["METADATA"]=layer_data["METADATA"]=service.contents[i].metadataUrls[0]['url'] if 0 in service.contents[i].metadataUrls else ""
         layer_data["UPDATE"]=""
-        layer_data["SERVICETYPE"]=service.identification.type
+        layer_data["SERVICETYPE"]="WFS"
         layer_data["MAX_ZOOM"]= "" #this is the map.geo.admin.ch map zoom at approx 1:20k
         layer_data["CENTER_LAT"]=(service[i].boundingBoxWGS84[1]+service[i].boundingBoxWGS84[3])/2
         layer_data["CENTER_LON"]=(service[i].boundingBoxWGS84[0]+service[i].boundingBoxWGS84[2])/2
