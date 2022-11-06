1d0b7b4b6a9436e3fb97a1a2e00942f4735400ee
David Oesch <david.oesch@gmail.com>
2022-11-06 15:20:33 +0100

    added KT_SZ

--- scraper/KT_SZ.py
+++ scraper/KT_SZ.py
@@ -1,21 +1,28 @@
 #Create OWSLIB configfile 
-
+#according to https://www.sz.ch/behoerden/vermessung-geoinformation/geoportal/daten-und-dienste.html/72-416-414-1762-1761
+import re
+regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 #SERVICE WMS
 def scrape(source,service,i,layertree, group,layer_data,prefix):
-    type=service.identification.type
- 
+    type=source['URL']
+#    print(i)
+    
     if "WMS" in type:
         layer_data["OWNER"]= source['Description']
         layer_data["TITLE"]= service.contents[i].title
         layer_data["NAME"]= service.contents[i].name
         layer_data["TREE"]= layertree
         layer_data["GROUP"]= group if group != 0 else ""
-        layer_data["ABSTRACT"]= service.contents[i].abstract
+        layer_data["ABSTRACT"]= service.contents[i].parent.abstract
         layer_data["KEYWORDS"]= ", ".join(service.contents[i].keywords+service.identification.keywords)
         layer_data["LEGEND"]= service.contents[i].styles['default']['legend'] if 'default' in service.contents[i].styles.keys() else ""
-        layer_data["CONTACT"]=service.provider.contact
+        layer_data["CONTACT"]="geoportal@sz.ch"
         layer_data["SERVICELINK"]=service.request
-        layer_data["METADATA"]=layer_data["METADATA"]=service.contents[i].metadataUrls[0]['url'] if 0 in service.contents[i].metadataUrls else ""
+        if service.contents[i].abstract is not None:
+            url = re.findall(regex,service.contents[i].abstract)
+            layer_data["METADATA"]=url[0][0] if len(url) == 1 else ""
+        else:
+            layer_data["METADATA"]=""
         layer_data["UPDATE"]=""
         layer_data["SERVICETYPE"]=service.identification.type
         layer_data["MAX_ZOOM"]= 7 #this is the map.geo.admin.ch map zoom at approx 1:20k
@@ -26,6 +33,7 @@ def scrape(source,service,i,layertree, group,layer_data,prefix):
             service.contents[i].id+"||"\
             +service.identification.version+"&swisssearch="+str(layer_data["CENTER_LAT"])+\
             "%20"+str(layer_data["CENTER_LON"])+"&zoom="+str(layer_data["MAX_ZOOM"])
+        
         return(layer_data)
     elif "WMTS" in type:
         layer_data["OWNER"]= source['Description']
@@ -33,7 +41,7 @@ def scrape(source,service,i,layertree, group,layer_data,prefix):
         layer_data["NAME"]= service.contents[i].name
         layer_data["TREE"]= layertree
         layer_data["GROUP"]= group if group != 0 else ""
-        layer_data["ABSTRACT"]= service.identification.abstract+" "+service.identification.accessconstraints
+        layer_data["ABSTRACT"]= service.identification.abstract
         layer_data["KEYWORDS"]= ", ".join(service.contents[i].keywords+service.identification.keywords)
         layer_data["LEGEND"]= service.contents[i].styles['default']['legend'] if 'legend' in service.contents[i].styles.keys() else ""
         layer_data["CONTACT"]=service.provider.name
@@ -42,10 +50,10 @@ def scrape(source,service,i,layertree, group,layer_data,prefix):
         layer_data["UPDATE"]=""
         layer_data["SERVICETYPE"]=service.identification.type
         layer_data["MAX_ZOOM"]= 7 #this is the map.geo.admin.ch map zoom at approx 1:20k
-        layer_data["CENTER_LAT"]=(service[i].boundingBoxWGS84[1]+service[i].boundingBoxWGS84[3])/2
-        layer_data["CENTER_LON"]=(service[i].boundingBoxWGS84[0]+service[i].boundingBoxWGS84[2])/2
-        layer_data["BBOX"]=' '.join([str(elem) for elem in (service.contents[i].boundingBoxWGS84)])
-        layer_data["MAPGEO"]= r""+prefix+"layers=WMTS||"+service.contents[i].id+"||"\
+        layer_data["CENTER_LAT"]="47.0561"
+        layer_data["CENTER_LON"]="8.696785"
+        layer_data["BBOX"]=" "
+        layer_data["MAPGEO"]= r""+prefix+"layers=WMTS||"+service.contents[i].title+"||"\
             +service.url+"&swisssearch="+str(layer_data["CENTER_LAT"])+\
             "%20"+str(layer_data["CENTER_LON"])+"&zoom="+str(layer_data["MAX_ZOOM"])
         return(layer_data)
@@ -58,11 +66,15 @@ def scrape(source,service,i,layertree, group,layer_data,prefix):
         layer_data["ABSTRACT"]= service.contents[i].abstract
         layer_data["KEYWORDS"]= ", ".join(service.contents[i].keywords+service.identification.keywords)
         layer_data["LEGEND"]= ""
-        layer_data["CONTACT"]=service.provider.contact.email
+        layer_data["CONTACT"]="geoportal@sz.ch"
         layer_data["SERVICELINK"]=service.url
-        layer_data["METADATA"]=layer_data["METADATA"]=service.contents[i].metadataUrls[0]['url'] if 0 in service.contents[i].metadataUrls else ""
+        if service.contents[i].abstract is not None:
+            url = re.findall(regex,service.contents[i].abstract)
+            layer_data["METADATA"]=url[0][0] if len(url) == 1 else ""
+        else:
+            layer_data["METADATA"]=""
         layer_data["UPDATE"]=""
-        layer_data["SERVICETYPE"]=service.identification.type
+        layer_data["SERVICETYPE"]="OGC:WFS"
         layer_data["MAX_ZOOM"]= "" #this is the map.geo.admin.ch map zoom at approx 1:20k
         layer_data["CENTER_LAT"]=(service[i].boundingBoxWGS84[1]+service[i].boundingBoxWGS84[3])/2
         layer_data["CENTER_LON"]=(service[i].boundingBoxWGS84[0]+service[i].boundingBoxWGS84[2])/2
