2bf81b07f165a53760020ae74aedd127d8c4c30c
David Oesch <david.oesch@gmail.com>
2022-11-27 18:07:02 +0100

    added KT UR

---
+++ scraper/KT_UR.py
@@ -0,0 +1,106 @@
+#Create OWSLIB configfile 
+# According to https://oereb.ur.ch/?basemap=AV&lat=46.87491213706447&lng=8.645001065327628&zoom=13.75 sources and www.geo.ur.ch
+#SERVICE WMS
+def scrape(source,service,i,layertree, group,layer_data,prefix):
+    type=source['URL']
+    #breakpoint()
+    if "wms" in type:
+        layer_data["OWNER"]= source['Description']
+        layer_data["TITLE"]= service.contents[i].title
+        layer_data["NAME"]= service.contents[i].name
+        layer_data["TREE"]= layertree
+        layer_data["GROUP"]= group if group != 0 else ""
+        if  service.contents[i].parent is not None and service.contents[i].parent.abstract is not None:
+            temp=str(service.contents[i].abstract)+" "+service.contents[i].parent.abstract
+        else:
+            temp=service.contents[i].abstract
+        layer_data["ABSTRACT"]=temp.replace('\r\n','') if temp is not None else ""
+        layer_data["KEYWORDS"]= ", ".join(service.contents[i].keywords+service.identification.keywords)
+        try: 
+            temp=service.contents[i].styles['default-style-'+i]['legend']
+        except KeyError:
+            try:
+                temp=service.contents[i].styles[i]['legend']
+            except KeyError:
+                temp=""
+        layer_data["LEGEND"]=temp
+        layer_data["CONTACT"]=service.provider.contact.email
+        layer_data["SERVICELINK"]=service.request
+        layer_data["METADATA"]=layer_data["METADATA"]=service.contents[i].metadataUrls[0]['url'] if 0 in service.contents[i].metadataUrls else ""
+        layer_data["UPDATE"]=service.updateSequence
+        layer_data["SERVICETYPE"]=service.identification.type
+        layer_data["MAX_ZOOM"]= 7 #this is the map.geo.admin.ch map zoom at approx 1:20k
+        layer_data["CENTER_LAT"]=(service[i].boundingBoxWGS84[1]+service[i].boundingBoxWGS84[3])/2
+        layer_data["CENTER_LON"]=(service[i].boundingBoxWGS84[0]+service[i].boundingBoxWGS84[2])/2
+        layer_data["BBOX"]=' '.join([str(elem) for elem in (service.contents[i].boundingBox)])
+        layer_data["MAPGEO"]= r""+prefix+"layers=WMS||"+service.contents[i].title+"||"+service.url+"?||"+\
+            service.contents[i].id+"||"\
+            +service.identification.version+"&swisssearch="+str(layer_data["CENTER_LAT"])+\
+            "%20"+str(layer_data["CENTER_LON"])+"&zoom="+str(layer_data["MAX_ZOOM"])
+        
+        return(layer_data)
+    elif "WMTS" in type:
+        layer_data["OWNER"]= source['Description']
+        layer_data["TITLE"]= service.contents[i].title
+        layer_data["NAME"]= service.contents[i].name
+        layer_data["TREE"]= layertree
+        layer_data["GROUP"]= group if group != 0 else ""
+        if  service.contents[i].parent is not None and service.contents[i].parent.abstract is not None:
+            temp=str(service.contents[i].abstract)+" "+service.contents[i].parent.abstract
+        else:
+            temp=service.contents[i].abstract
+        layer_data["ABSTRACT"]=temp.replace('\r\n','') if temp is not None else ""
+        layer_data["KEYWORDS"]= ", ".join(service.contents[i].keywords+service.identification.keywords)
+        layer_data["LEGEND"]= ""#service.contents[i].styles['default-style-'+i]['legend']
+        layer_data["CONTACT"]=service.provider.name
+        layer_data["SERVICELINK"]=service.url
+        layer_data["METADATA"]=service.serviceMetadataURL
+        layer_data["UPDATE"]=service.updateSequence
+        layer_data["SERVICETYPE"]=service.identification.type
+        layer_data["MAX_ZOOM"]= 7 #this is the map.geo.admin.ch map zoom at approx 1:20k
+        layer_data["CENTER_LAT"]=(service[i].boundingBoxWGS84[1]+service[i].boundingBoxWGS84[3])/2
+        layer_data["CENTER_LON"]=(service[i].boundingBoxWGS84[0]+service[i].boundingBoxWGS84[2])/2
+        layer_data["BBOX"]=' '.join([str(elem) for elem in (service.contents[i].boundingBoxWGS84)])
+        layer_data["MAPGEO"]= r""+prefix+"layers=WMTS||"+service.contents[i].id+"||"\
+            +service.url+"&swisssearch="+str(layer_data["CENTER_LAT"])+\
+            "%20"+str(layer_data["CENTER_LON"])+"&zoom="+str(layer_data["MAX_ZOOM"])
+        
+        return(layer_data)
+    elif "wfs" in type:
+        layer_data["OWNER"]= source['Description']
+        layer_data["TITLE"]= service.contents[i].title
+        layer_data["NAME"]= service.contents[i].id
+        layer_data["TREE"]= layertree
+        layer_data["GROUP"]= group if group != 0 else ""
+        if  service.contents[i].parent is not None and service.contents[i].parent.abstract is not None:
+            temp=str(service.contents[i].abstract)+" "+service.contents[i].parent.abstract
+        else:
+            temp=service.contents[i].abstract
+        #temp=temp.replace('\n\n','')
+        layer_data["ABSTRACT"]=temp.replace('\r\n','') if temp is not None else ""
+        layer_data["KEYWORDS"]= ", ".join(service.contents[i].keywords+service.identification.keywords)
+        try: 
+            temp=service.contents[i].styles['default-style-'+i]['legend']
+        except KeyError:
+            try:
+                temp=service.contents[i].styles[i]['legend']
+            except KeyError:
+                temp=""
+        layer_data["LEGEND"]=temp
+        layer_data["CONTACT"]=service.provider.contact.email
+        layer_data["SERVICELINK"]=service.url
+        layer_data["METADATA"]=layer_data["METADATA"]=service.contents[i].metadataUrls[0]['url'] if 0 in service.contents[i].metadataUrls else ""
+        layer_data["UPDATE"]=service.updateSequence
+        layer_data["SERVICETYPE"]=service.identification.type
+        layer_data["MAX_ZOOM"]= "" #this is the map.geo.admin.ch map zoom at approx 1:20k
+        layer_data["CENTER_LAT"]=(service[i].boundingBoxWGS84[1]+service[i].boundingBoxWGS84[3])/2
+        layer_data["CENTER_LON"]=(service[i].boundingBoxWGS84[0]+service[i].boundingBoxWGS84[2])/2
+        layer_data["BBOX"]=' '.join([str(elem) for elem in (service.contents[i].boundingBoxWGS84)])
+        #breakpoint()
+        return(layer_data)               
+    elif "STAC" in type:
+        print("STAC detetcted ..add config")
+    else:
+        return(False)        
+
+    
