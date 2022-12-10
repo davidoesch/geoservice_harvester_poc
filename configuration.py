a6eaaf5200b6f1e1d2b5ac2283641610a322a542
David Oesch <david.oesch@gmail.com>
2022-12-10 21:37:46 +0100

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -4,6 +4,7 @@ SOURCE_COLLECTION_CSV = "sources.csv"
 SOURCE_COLLECTION_VERSION = {"KT_AI":"1.3.0","KT_AR":"1.3.0","Geodienste":"1.3.0"}
 SOURCE_SCRAPER_DIR = os.path.join("scraper")
 GEOSERVICES_CH_CSV = os.path.join("data", "geoservices_CH.csv")
+GEOSERVICES_STATS_CH_CSV=os.path.join("data", "geoservices_stats_CH.csv")
 GEODATA_CH_CSV = os.path.join("data", "geodata_CH.csv")
 GEODATA_SIMPLE_CH_CSV = os.path.join("data", "geodata_simple_CH.csv")
 LOG_FILE = os.path.join("tools", "debug.log")
