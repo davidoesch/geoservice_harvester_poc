davidoesch/geoservice_harvester_poc

commit a63266f06949b5d4df51f542a41ec2772f45f5ea

Remove concatenation
This remove concatenation for abstract, access constraints and keywords.

scraper/default.py
@@ -90,28 +90,12 @@ def scrape(source,service,i,layertree, group,layer_data,prefix):
         layer_data["GROUP"] = ""
 
     #abstract
-    temp = service.contents[i].abstract if hasattr(service.contents[i], 'abstract') and service.contents[i].abstract is not None else ""
-    if hasattr(service.contents[i], 'parent') and hasattr(service.contents[i].parent, 'abstract') and service.contents[i].parent.abstract is not None:
-        temp += " " + service.contents[i].parent.abstract
-    if hasattr(service.identification, 'accessconstraints') and service.identification.accessconstraints is not None:
-        temp += " " + service.identification.accessconstraints
+    temp = service.contents[i].abstract
     layer_data["ABSTRACT"] = remove_newline(temp) if temp else ""
 
-
     #keywords
-    if service.contents[i].keywords and service.identification.keywords:
-        keywords1 = [k for k in service.contents[i].keywords if k is not None]
-        keywords2 = [k for k in service.identification.keywords if k is not None]
-        layer_data["KEYWORDS"] = ", ".join(keywords1 + keywords2)
-    elif service.contents[i].keywords:
-        keywords = [k for k in service.contents[i].keywords if k is not None]
-        layer_data["KEYWORDS"] = ", ".join(keywords)
-    elif service.identification.keywords:
-        keywords = [k for k in service.identification.keywords if k is not None]
-        layer_data["KEYWORDS"] = ", ".join(keywords)
-    else:
-        layer_data["KEYWORDS"] = ""
-
+    keywords = [k for k in service.contents[i].keywords]
+    layer_data["KEYWORDS"] = ", ".join(keywords)    
     
     #legend    
     if service.contents[i].styles is not None:

commit 4fb1f4fb7a4baf8b0562dd98c9891269443cbb76

Fix Missing layers #31
This fixes Missing layers #31 - https://github.com/davidoesch/geoservice_harvester_poc/issues/31 and as well a bug in AR AI introduced in https://github.com/davidoesch/geoservice_harvester_poc/commit/a63266f06949b5d4df51f542a41ec2772f45f5ea

- Groups layer where data is shown in a GetMap request are added as a layer

scraper.py
@@ -202,9 +202,9 @@ def get_service_info(source):
         layers=list(service.contents)
         layers_done=[]
         for i in layers:
-            
+
             #check if we did not yet have processed that layer as child before
-            #print(i)
+            
             #breakpoint()
             
             if i not in layers_done :           
@@ -215,6 +215,29 @@ def get_service_info(source):
                 else:
                    children=0
                 #breakpoint()
+           
+                #get root layer / extracting the description for simple layer
+                if service.contents[i].id not in layers_done :
+                    # Some root WMS layers are blocked so no get map is possible, so we check if we can load them as TOPIC (aka al children layer active)
+                    if "WMS" in source['URL'] or "wms" in source['URL']:
+                        try:
+                            #check if root layer is loadable, by trying to call a Get Map, if it is blocked it will raise an error
+                            service.getmap(layers=[i], srs='EPSG:4326', bbox=(service.contents[i].boundingBoxWGS84[0],service.contents[i].boundingBoxWGS84[1],service.contents[i].boundingBoxWGS84[2],service.contents[i].boundingBoxWGS84[3]), size=(256,256), format='image/png', transparent=True, timeout=20)
+                            #then extract abstract etc
+                            layertree= source['Description']+"/"+service.identification.title+"/"+i.replace('"', '') if service.identification.title is not None else source['Description']+"/"+i.replace('"', '')
+                            write_service_info(source,service,(service.contents[i].id),layertree,group=i)                
+                            layers_done.append(service.contents[i].id)
+                        except Exception as e:
+                            # Check if the exception indicates that the request was not allowed or forbidden
+                            if any([msg in str(e) for msg in service.exceptions]):
+                                print(i+' GetMap request is blocked for this layer')
+                            else:
+                                print(i+' Unknown error:', e)
+                    else:         
+                            layertree= source['Description']+"/"+service.identification.title+"/"+i.replace('"', '') if service.identification.title is not None else source['Description']+"/"+i.replace('"', '')
+                            write_service_info(source,service,(service.contents[i].id),layertree,group=i)                
+                            layers_done.append(service.contents[i].id)
+
                 #check for parent layer, when yes use  it for tree
                 if children > 0 :
                     #print(i+" processing parent layer")
@@ -225,14 +248,8 @@ def get_service_info(source):
                             #breakpoint()
                             write_service_info(source,service,(service.contents[i]._children[j].id),layertree,group=i)
                             layers_done.append(service.contents[i]._children[j].id)
+         
 
-
-                else:
-                    #Extracting the description for simple layer
-                    #print(i+" processing NORMAL layer")
-                    layertree= source['Description']+"/"+service.identification.title if service.identification.title is not None else source['Description']+"/"
-                    write_service_info(source,service,i,layertree,group=0)
-                    layers_done.append(i)
 
             else:
                 print(i+" already processed")

scraper/default.py
@@ -1,5 +1,5 @@
 """
-Title: deafult scraper
+Title: default scraper
 Author: David Oesch
 Date: 2022-11-05
 Purpose: This script is be a part of a larger script that scrapes metadata information from 
@@ -62,6 +62,7 @@ def scrape(source,service,i,layertree, group,layer_data,prefix):
     """
     type=source['URL']
     #breakpoint()
+
     #owner
     layer_data["OWNER"]= source['Description']
     
@@ -94,7 +95,7 @@ def scrape(source,service,i,layertree, group,layer_data,prefix):
     layer_data["ABSTRACT"] = remove_newline(temp) if temp else ""
 
     #keywords
-    keywords = [k for k in service.contents[i].keywords]
+    keywords = [k for k in service.contents[i].keywords if k is not None]
     layer_data["KEYWORDS"] = ", ".join(keywords)    
     
     #legend    